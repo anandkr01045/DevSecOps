1223ba0622867398c6e49ad5730fb609c602685b
anandkr01045 <143266465+anandkr01045@users.noreply.github.com>
2023-08-27 21:06:36 +0530

    Update main.tf with resources

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "myvnet" {
   name                = "myvnet-1"
   address_space       = ["10.0.0.0/16"]
   location            = "Central US"
-  resource_group_name = "1-901e0083-playground-sandbox"
+  resource_group_name = "1-d297f585-playground-sandbox"
   tags = {
     Environment = "Dev"
     Team        = "DevSecOps-Team"
@@ -37,8 +37,33 @@ resource "azurerm_virtual_network" "myvnet" {
 # Create Subnet
 resource "azurerm_subnet" "mysubnet" {
   name                 = "mysubnet-1"
-  resource_group_name  = "1-901e0083-playground-sandbox"
+  resource_group_name  = "1-d297f585-playground-sandbox"
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Create Public IP Address
+resource "azurerm_public_ip" "mypublicip" {
+  name                = "mypublicip-1"
+  resource_group_name = "1-d297f585-playground-sandbox"
+  location            = "Central US"
+  allocation_method   = "Static"
+  domain_name_label = "app1-vm-${random_string.myrandom.id}"
+  tags = {
+    environment = "Dev"
+  }
+}
+
+# Create Network Interface
+resource "azurerm_network_interface" "myvmnic" {
+  name                = "vmnic"
+  location            = "Central US"
+  resource_group_name = "1-d297f585-playground-sandbox"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.mypublicip.id 
+  }
+}
